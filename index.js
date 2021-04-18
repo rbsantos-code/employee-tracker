ed2f5539fbbe60aa5470dc2bc77e02a70ba97b71
rbsantos-code <richardbriansantos@richards-mbp.attlocal.net>
2021-04-18 16:42:38 -0700

    working on init function

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
 // Dependencies
+const { beforeAll } = require('@jest/globals');
 const inquirer = require('inquirer');
 const db = require('./db');
+
+// Function Section
+function init() {
+
+    // function to team prompt
+    function teamPrompt() {
+        console.log("let's go team!");
+        inquirer.prompt([
+            {
+                // ask choice section
+                type: 'list',
+                name: 'viewTeam',
+                message: 'What do you want to do?',
+                choices: [ 
+                    'View all departments',
+                    'View all roles',
+                    'View all employees',
+                    'Nothing'
+                ]
+
+            }
+        ])
+        // start section depending on choice
+        .then(userChoice => {
+            switch (userChoice.viewTeam) {
+                case 'View all departments':
+                    // add function
+                    break;
+                case 'View all roles':
+                    //add function
+                    break;
+                case 'View all employees':
+                    // add function
+                    break;
+                case 'Nothing':
+                    // add function
+            }
+        });
+    }
+
+
+}
