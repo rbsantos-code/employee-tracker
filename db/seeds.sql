570f3117dd3048d3a8932889777c24cccf4ca98f
rbsantos-code <richardbriansantos@richards-mbp.attlocal.net>
2021-04-18 14:25:44 -0700

    add values roles

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,16 @@ VALUES
     ('Engineering'),
     ('Finance'),
     ('Legal');
+
+
+INSERT INTO roles (title, salary)
+VALUES
+    ('Manager', 150000),
+    ('Sales Lead', 100000),
+    ('Sales', 70000),
+    ('Senior Engineer', 250,000),
+    ('Engineer', 100000),
+    ('Chief Finance Analyst', 300,000),
+    ('Financial Analyst, 120000'),
+    ('Legal Team Lead', 230000),
+    ('Lawyer', 180000);
