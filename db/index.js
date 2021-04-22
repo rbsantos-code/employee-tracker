be4a9f9b0a7f0539e47c6f522bd6a234105cb37e
rbsantos-code <richardbriansantos@Richards-MacBook-Pro.local>
2021-04-21 20:12:21 -0700

    create db index

--- db/index.js
+++ db/index.js
@@ -1,1 +1,17 @@
-const 
+const connection = require('./connection');
+
+class dataBase {
+    constructor(connection){
+        this.connection = connection;
+    }
+
+    // Show departments 
+    departments() {
+        return this.connection.promise().query(
+            'SELECT departments.id, departments.department_name AS department FROM departments;'
+        );
+    }
+}
+
+
+module.exports = new dataBase(connection);
